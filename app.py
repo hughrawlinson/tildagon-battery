8ff5c2caa15a75b6cbc13117ea2c3ce7b01399c6
Hugh Rawlinson <hughr2005@gmail.com>
2024-06-01 17:41:15 +0100

    Exiting the app

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from app import App
 from app_components import clear_background
 from power import BatteryLevel
+from events.input import Buttons, BUTTON_TYPES
 
 class Battery(App):
   def __init__(self):
-    pass
+    self.button_states = Buttons(self)
 
-  def update(self, delta):
-    pass
+  def update(self, delta):if self.button_states.get(BUTTON_TYPES["CANCEL"]):
+    # The button_states do not update while you are in the background.
+    # Calling clear() ensures the next time you open the app, it stays open.
+    # Without it the app would close again immediately.
+    self.button_states.clear()
+    self.minimise()
 
   def draw(self, ctx):
     ctx.save()

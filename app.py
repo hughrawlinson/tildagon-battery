3306fe50e3a2a659386a669f0c1b0791c3117b4e
Hugh Rawlinson <hughr2005@gmail.com>
2024-06-02 18:34:30 +0100

    Fix percentage

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class Battery(App):
     ctx.text_align = ctx.CENTER
     ctx.text_baseline = ctx.MIDDLE
     clear_background(ctx)
-    label = f"{(BatteryLevel()/106.25):.2f}%"
+    label = f"{(100*BatteryLevel()/106.25):.2f}%"
     ctx.move_to(0, 0).gray(1).text(label)
     ctx.restore()
 
